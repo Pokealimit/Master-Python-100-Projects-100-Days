305f202197d39e1950c6e93e74e1bd22bbcc884d
Pokealimit <pokealimit@gmail.com>
2021-09-03 23:47:42 +0800

    added day 17

---
+++ Day_17_QuizGame/question_model.py
@@ -0,0 +1,6 @@
+class Question:
+
+    def __init__(self, text, answer):
+        """ Create a Question object with text(Question) and answer """
+        self.text = text
+        self.answer = answer
