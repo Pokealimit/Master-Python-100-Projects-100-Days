a12da5141ac23eebf024439e0d382d9c0d6b56bc
Pokealimit <pokealimit@gmail.com>
2021-09-04 15:25:16 +0800

    added initial prac of day 18

---
+++ Day_18_TurtleGUI/differentShape.py
@@ -0,0 +1,77 @@
+from turtle import Turtle, Screen, colormode, forward, right
+from random import randint
+
+class differentShape:
+    def __init__(self):
+        self.tim = Turtle()
+        colormode(255)
+    
+    def drawTriangle(self):
+        self.tim.pencolor(self.random_pen_colour())
+        for _ in range(3):
+            self.tim.forward(100)
+            self.tim.right(120)
+
+    def drawSquare(self):
+        self.tim.pencolor(self.random_pen_colour())
+        for _ in range(4):
+            self.tim.forward(100)
+            self.tim.right(90)
+
+    def drawPentagon(self):
+        self.tim.pencolor(self.random_pen_colour())
+        for _ in range(5):
+            self.tim.forward(100)
+            self.tim.right(72)
+
+    def drawHexagon(self):
+        self.tim.pencolor(self.random_pen_colour())
+        for _ in range(6):
+            self.tim.forward(100)
+            self.tim.right(60)
+
+    def drawHeptagon(self):
+        self.tim.pencolor(self.random_pen_colour())
+        for _ in range(7):
+            self.tim.forward(100)
+            self.tim.right(51.43)
+    
+    def drawOctagon(self):
+        self.tim.pencolor(self.random_pen_colour())
+        for _ in range(8):
+            self.tim.forward(100)
+            self.tim.right(45)
+
+    def drawNonagon(self):
+        self.tim.pencolor(self.random_pen_colour())
+        for _ in range(9):
+            self.tim.forward(100)
+            self.tim.right(40)
+
+    def drawDecagon(self):
+            self.tim.pencolor(self.random_pen_colour())
+            for _ in range(10):
+                self.tim.forward(100)
+                self.tim.right(36)
+
+    def random_pen_colour(self) -> tuple:
+        R = randint(0, 255)
+        G = randint(0, 255)
+        B = randint(0, 255)
+
+        return (R,G,B)
+        
+
+
+if __name__ == "__main__":
+    timmy = differentShape()
+    timmy.drawTriangle()
+    timmy.drawSquare()
+    timmy.drawPentagon()
+    timmy.drawHexagon()
+    timmy.drawHeptagon()
+    timmy.drawOctagon()
+    timmy.drawNonagon()
+    timmy.drawDecagon()
+    screen = Screen()
+    screen.exitonclick()
---
+++ Day_18_TurtleGUI/drawSquare.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("black")
+timmy_the_turtle.penup()
+timmy_the_turtle.forward(50)
+timmy_the_turtle.right(90)
+timmy_the_turtle.pendown()
+timmy_the_turtle.forward(50)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(50)
+
+screen = Screen()
+screen.exitonclick()
