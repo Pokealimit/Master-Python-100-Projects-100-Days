e0e44dda1bfbf569235d0201d4ee6b03752da184
Pokealimit <pokealimit@gmail.com>
2021-09-04 18:35:11 +0800

    initial day18 hist proj

---
+++ Day_18_TurtleGUI/hirst_painting.py
@@ -0,0 +1,34 @@
+import colorgram
+import turtle as t
+import random as r
+import numpy as np
+
+colors = colorgram.extract('hirst_spot_painting.png',  2 ** 32)
+
+color_list = []
+for color in colors:
+    R = color.rgb.r
+    G = color.rgb.g
+    B = color.rgb.b
+    color_list.append((R,G,B))
+
+for _ in range(4):
+    color_list.pop(0)
+
+tim = t.Turtle()
+t.colormode(255)
+tim.penup()
+inital_y = -235
+tim.setpos(-235, inital_y)
+
+for y in range(10):
+    for x in range(10):
+        tim.color(r.choice(color_list))
+        tim.dot(20)
+        tim.forward(50)
+
+    inital_y += 50
+    tim.setpos(-235, inital_y)
+
+screen = t.Screen()
+screen.exitonclick()
