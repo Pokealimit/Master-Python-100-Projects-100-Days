0756cb6479bb9828383ad2c476edc1ff7ef4c451
Pokealimit <pokealimit@gmail.com>
2021-09-05 13:40:00 +0800

    day 20 snake to move forward

--- Day_20_SnakeGame_P1/main.py
+++ Day_20_SnakeGame_P1/main.py
@@ -1,20 +1,35 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 initial_coord = [(0,0),(-20,0),(-40,0)]
-turtle_list = []
+segments = []
 
 for position in initial_coord:
-    tim = Turtle("square")
-    tim.color("white")
-    tim.penup()
-    tim.goto(position)
-    turtle_list.append(tim)
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
 
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    # update each segment position to the segment before it
+    for seg_num in range(len(segments)-1, 0, -1):
+        new_x = segments[seg_num-1].xcor()
+        new_y = segments[seg_num-1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+    # first segment(head) move forward
+    segments[0].forward(20)
 
 screen.exitonclick()
