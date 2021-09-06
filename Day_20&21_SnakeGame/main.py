b9eb34ab2b2979758809398a68d62b4f540ddfab
Pokealimit <pokealimit@gmail.com>
2021-09-06 23:39:24 +0800

    added highscore for snake day 24

--- Day_20&21_SnakeGame/main.py
+++ Day_20&21_SnakeGame/main.py
@@ -34,13 +34,14 @@ while game_is_on:
 
     # Detection collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
+        scoreboard.reset()
+        snake.reset()
 
     # Detection collision with tail.
     for segment in snake.segments[1:]:  # Ignore first segment which is the head
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset
+            snake.reset()
 
 scoreboard.game_over()
 screen.exitonclick()
--- Day_20&21_SnakeGame/scoreboard.py
+++ Day_20&21_SnakeGame/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", "r") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -16,11 +18,15 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score = {self.score}   High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+        with open("data.txt", "w") as data:
+            data.write(f"{self.high_score}")
 
     def increase_score(self):
         self.score += 1
