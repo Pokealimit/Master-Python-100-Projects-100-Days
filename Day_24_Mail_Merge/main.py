061926303d45e0f962c292d2ed9ede1593bffaa3
Pokealimit <pokealimit@gmail.com>
2021-09-07 00:08:50 +0800

    completed day 24 mail merge

--- Day_24_Mail_Merge/main.py
+++ Day_24_Mail_Merge/main.py
@@ -1,4 +1,4 @@
-#TODO: Create a letter using starting_letter.txt 
+#TODO: Create a letter using starting_letter.txt
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
@@ -6,3 +6,25 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+# Opening Letter Format
+with open("./Input/Letters/starting_letter.txt", 'r') as start:
+    starting_letter = start.read()
+# print(starting_letter)
+
+# Open Name List
+with open("./Input/Names/invited_names.txt", "r") as names:
+    name_list = names.readlines()
+# print(name_list)
+
+# Remove \n from names in name list
+new_name_list = []
+for name in name_list:
+    new_name_list.append(name.strip())
+# print(new_name_list)
+
+# Write customised letter for everyone in name list in Output/ReadyToSend Folder
+for name in new_name_list:
+    new_letter = starting_letter.replace("[name]", name)
+    with open(f"./Output/ReadyToSend/letter_for_{name}.txt", "w") as letter:
+        letter.write(new_letter)
