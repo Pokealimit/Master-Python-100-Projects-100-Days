81b5cbea242a09ad6d79a4031800ed142d9e5251
Pokealimit <pokealimit@gmail.com>
2021-09-09 23:23:58 +0800

    added layout

--- Day_28_Pomodoro/main.py
+++ Day_28_Pomodoro/main.py
@@ -20,12 +20,26 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Timer Header Label
+timer_header = Label(text="Timer", fg=GREEN, bg=YELLOW,font=(FONT_NAME, 50))
+timer_header.grid(column=1, row=0)
+
 # Create tomato image
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 # To make tomato be in center of canvas (half the coordinates in canvas)
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+# Buttons
+start_btn = Button(text="Start", highlightthickness=0, font=(FONT_NAME, 18))
+start_btn.grid(column=0, row=2)
+reset_btn = Button(text="Reset", highlightthickness=0, font=(FONT_NAME, 18))
+reset_btn.grid(column=2, row=2)
+
+# Pomodoro Ticks
+ticks = Label(text="✔", fg=GREEN, bg=YELLOW, font=(24))
+ticks.grid(column=1, row=3)
 
 window.mainloop()
