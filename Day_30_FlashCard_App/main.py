1c115e3f313ca20455b04593615593ce1c17c02e
Pokealimit <pokealimit@gmail.com>
2021-09-10 23:16:29 +0800

    updated flipping and changing card

--- Day_30_FlashCard_App/main.py
+++ Day_30_FlashCard_App/main.py
@@ -8,43 +8,36 @@ timer = None
 
 # ---------------------------- CHANGE CARD ------------------------------- #
 def change_card():
-    global current
-    current = choice(french_words_list)
-    # language = next(iter(current))
-    # word = current[language]
-    word = current["French"]
+    global current_card, flip_timer
+    # Cancel the function call to flip card if user change card before the english word is revealed
+    window.after_cancel(flip_timer)
+    current_card = choice(french_words_list)
     canvas.itemconfig(canvas_iamge, image=card_image_front)
     canvas.itemconfig(Language_text, text="French", fill="black")
-    canvas.itemconfig(word_text, text=f"{word}", fill="black")
-    count_down(3)
+    canvas.itemconfig(word_text, text=current_card["French"], fill="black")
+    flip_timer = window.after(3000,flip_card)
 
 # ---------------------------- READ CSV ------------------------------- #
 french_words = pd.read_csv("data/french_words.csv")
 french_words_list = french_words.to_dict(orient="records")
 
-# ---------------------------- TIMER ------------------------------- #
+# ---------------------------- FLIP CARD ------------------------------- #
 
-def count_down(count):
-    global current
-    if count > 0:
-        global timer
-        timer = window.after(1000, count_down, count - 1)
+def flip_card():
+    global current_card
 
-    elif count == 0:
-        word = current["English"]
-        canvas.itemconfig(canvas_iamge, image=card_image_back)
-        canvas.itemconfig(Language_text, text="English", fill="white")
-        canvas.itemconfig(word_text, text=f"{word}", fill="white")
+    canvas.itemconfig(canvas_iamge, image=card_image_back)
+    canvas.itemconfig(Language_text, text="English", fill="white")
+    canvas.itemconfig(word_text, text=current_card["English"], fill="white")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-current = choice(french_words_list)
-# language = next(iter(current))
-# word = current[language]
-word = current["French"]
+current_card = choice(french_words_list)
+
+flip_timer = window.after(3000,flip_card)
 
 # Flash Card Image
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
@@ -55,7 +48,7 @@ canvas_iamge = canvas.create_image(400, 263, image=card_image_front)
 
 # Card Text
 Language_text = canvas.create_text(400, 150, text="French", fill="black", font=("Ariel", 40, "italic"))
-word_text = canvas.create_text(400, 263, text=f"{word}", fill="black", font=("Ariel", 60, "bold"))
+word_text = canvas.create_text(400, 263, text=current_card["French"], fill="black", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 
@@ -66,7 +59,5 @@ wrong_button.grid(column=0, row=1)
 tick_button_image = PhotoImage(file="images/right.png")
 tick_button = Button(image=tick_button_image, highlightthickness=0, command=change_card)
 tick_button.grid(column=1, row=1)
-
-count_down(3)
 
 window.mainloop()
